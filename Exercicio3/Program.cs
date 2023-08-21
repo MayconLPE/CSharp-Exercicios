aebe7f25520a89fd322475b22f8d4f8aa7dde74e
MayconL27 <mayconleandro2008@gmail.com>
2023-08-21 20:10:10 -0300

    Exercicio3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 3) Faça um programa que: a) Leia o nome; b) Leia o sobrenome; 
+        // c) Concatene o nome com o sobrenome; d) Apresente o nome completo.
+
+        String nome = "Maycon";
+        String sobrenome = "Leandro";
+
+        Console.WriteLine(nome + " " + sobrenome);
+    }
+}
