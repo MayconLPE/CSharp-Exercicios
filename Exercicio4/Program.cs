6be3ae2f4f1eec06679d5bf95e2938cc0663b554
MayconL27 <mayconleandro2008@gmail.com>
2023-08-22 08:22:00 -0300

    Exercicio4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 4) Escrever um programa que leia o nome de um vendedor, o seu salário fixo e o
+        // total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor
+        // ganha 15% de comissão sobre suas vendas efetuadas, informar o seu nome, o salário
+        // fixo e salário no final do mês.
+
+        String nomeVendedor = "Maycon";
+        double salarioFixo = 5000;
+        double totalVendas = 2000;
+        double comissao = (totalVendas/100)*15; // 15%
+        double salarioFinal = salarioFixo+comissao;
+    
+        Console.WriteLine("Nome: " + nomeVendedor);
+        Console.WriteLine("Salario fixo: " + salarioFixo);
+        Console.WriteLine("Salario final: " + salarioFinal);
+    }
+}
