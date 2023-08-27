057e2be8885af32a6b2febb5b9a3f25833a51464
MayconL27 <mayconleandro2008@gmail.com>
2023-08-27 20:00:47 -0300

    Exercicio7 e 8

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* 7) Faça um programa que leia uma temperatura em graus Celsius e apresente-a
+            convertida em graus Fahrenheit. A fórmula de conversão é: F = (9 * C + 160) / 5, na qual
+            F é a temperatura em Fahrenheit e C é a temperatura em Celsius; */
+            
+        Console.WriteLine("Hello, World!");
+    }
+}
