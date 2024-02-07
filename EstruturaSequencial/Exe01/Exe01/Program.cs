30074b22372c11bdc695d82ea23c7f9ba8af3720
MayconL27 <mayconleandro2008@gmail.com>
2024-02-07 08:39:11 -0300

    Estrutura Sequancial Exe01

---
+++ EstruturaSequencial/Exe01/Exe01/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1. Faça um Programa que mostre a mensagem "Alo mundo" na tela
+        Console.WriteLine("Hello, World!");
+    }
+}
