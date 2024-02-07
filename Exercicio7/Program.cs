1c03aca69bb2acbb5985bd6f507b43643a31f7b0
MayconL27 <mayconleandro2008@gmail.com>
2024-02-07 08:31:15 -0300

    Exercicio7, completo

--- Exercicio7/Program.cs
+++ Exercicio7/Program.cs
@@ -5,7 +5,15 @@
         /* 7) Faça um programa que leia uma temperatura em graus Celsius e apresente-a
             convertida em graus Fahrenheit. A fórmula de conversão é: F = (9 * C + 160) / 5, na qual
             F é a temperatura em Fahrenheit e C é a temperatura em Celsius; */
-            
-        Console.WriteLine("Hello, World!");
+
+        double GrausC, GrausF;    
+        Console.WriteLine("Digite a Temperatura em Graus Celsius:");
+        GrausC = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(GrausC);
+
+        GrausF = (9 * GrausC + 160) / 5;
+       
+        Console.WriteLine("A temperatura " + GrausC + "°F convertido \nem graus Celsius é: " + GrausF);
     }
 }
