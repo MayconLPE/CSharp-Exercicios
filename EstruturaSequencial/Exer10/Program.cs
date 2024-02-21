a292ac179d382ee8f629eb4b404b8a06d377aff5
MayconL27 <mayconleandro2008@gmail.com>
2024-02-21 17:10:33 -0300

    Exer10

---
+++ EstruturaSequencial/Exer10/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit. F = (9 * C + 160) / 5
+
+        double GrausC, GrausF;    
+        Console.WriteLine("Digite a Temperatura em Graus Celsius:");
+        GrausC = double.Parse(Console.ReadLine());
+        Console.WriteLine(GrausC);
+        GrausF = (9 * GrausC + 160) / 5;
+       
+        Console.WriteLine("A temperatura " + GrausC + "°F convertido \nem graus Celsius é: " + GrausF);
+    }
+}
