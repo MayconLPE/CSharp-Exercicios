c23aa62931e28467dd922c731ec0d2c5073b4910
MayconL27 <mayconleandro2008@gmail.com>
2024-02-21 15:46:24 -0300

    Exer09

---
+++ EstruturaSequencial/Exer09/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius. C = (5 * (F-32) / 9).
+        double grausF, grausC;
+
+        Console.WriteLine("Digite a temperatura em graus Farenheit: ");
+        grausF = double.Parse(Console.ReadLine());
+
+        grausC = (5 * (grausF - 32)/9);
+
+        Console.WriteLine("Graus Celsius: " + grausC.ToString("F"));
+    }
+}
