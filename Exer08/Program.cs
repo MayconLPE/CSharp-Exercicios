a390c217148481858c9e1c25bdcdf8543467f7d0
MayconL27 <mayconleandro2008@gmail.com>
2024-02-21 15:24:23 -0300

    Exer08

---
+++ Exer08/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* 8. Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+        Calcule e mostre o total do seu salário no referido mês. */
+
+        double salarioHoras, horasTrabalhadas, salarioTotal;
+
+        Console.WriteLine("Quanto você ganha por hora?");
+        salarioHoras = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite as horas trabalhadas:");
+        horasTrabalhadas = double.Parse(Console.ReadLine());
+        salarioTotal = salarioHoras * horasTrabalhadas;
+
+        Console.WriteLine("Salario total ganho por mês: R$ " + salarioTotal);
+
+
+        
+    }
+}
