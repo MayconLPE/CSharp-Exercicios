f21003d40fa79b03aacd331de8d0ef50ec26cf48
MayconL27 <mayconleandro2008@gmail.com>
2024-02-26 15:32:54 -0300

    Exer 12 e 13

---
+++ EstruturaSequencial/Exer12/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 12. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, 
+        // usando a seguinte fórmula: (72.7*altura) - 5812.
+
+        double altura, pesoIdeal;
+    
+        Console.WriteLine("Digite sua altura: ");
+        altura = double.Parse(Console.ReadLine());
+        pesoIdeal = (72.7*altura) - 58;
+
+        Console.WriteLine("Seu peso ideal = " + pesoIdeal);
+    }
+}
---
+++ EstruturaSequencial/Exer13/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+            Para homens: (72.7*h) - 58
+            Para mulheres: (62.1*h) - 44.7 */
+
+        double altura, pesoIdeal;
+        int selecao;
+    
+        Console.WriteLine("Digite sua altura: ");
+        altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Selecione a opção 1 - Homem ou 2- Mulher");
+        selecao = int.Parse(Console.ReadLine());
+
+        switch (selecao)
+        {
+            case 1:
+            pesoIdeal = (72.7*altura) - 58;
+            Console.WriteLine("Homem: " + pesoIdeal);
+            break;
+
+            case 2:
+            pesoIdeal = (62.1*altura) - 44.7;
+            Console.WriteLine("Mulher: " + pesoIdeal);
+            break;    
+
+            default:
+            Console.WriteLine("Opção invalida!");
+            break;
+        }
+        pesoIdeal = (72.7*altura) - 58;
+
+    }
+}
