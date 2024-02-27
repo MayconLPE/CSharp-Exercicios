0127263e5020b085545ab1ed74647fa2898d0799
MayconL27 <mayconleandro2008@gmail.com>
2024-02-27 11:22:32 -0300

    Exer 14 ao 18

---
+++ EstruturaSequencial/Exer14/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*oão Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda
+            vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos)
+            deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso
+            de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da
+            multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.
+        */
+        double pesoPeixes;
+
+        Console.WriteLine("Digite o peso dos Peixes: ");
+        pesoPeixes = double.Parse(Console.ReadLine());
+
+        if (pesoPeixes > 50) {
+			Double multaValor = (pesoPeixes - 50)* 4; // Valor de multa.
+			Console.WriteLine("Peso ultrapassado valor a ser pago com multa é: R$ " + multaValor);
+		}
+		else {
+			Console.WriteLine("Peso não ultrapassado sem pagamentos de multa."); // Valor sem multa.
+		}
+    }
+}
---
+++ EstruturaSequencial/Exer15/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* 15. Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total
+            do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o
+            sindicato, faça um programa que nos dê:
+            salário bruto.
+            quanto pagou ao INSS.
+            quanto pagou ao sindicato.
+            o salário líquido.
+            calcule os descontos e o salário líquido, conforme a tabela abaixo:
+            + Salário Bruto : R$
+            - IR (11%) : R$
+            - INSS (8%) : R$
+            - Sindicato ( 5%) : R$
+            = Salário Liquido : R$
+            Obs.: Salário Bruto - Descontos = Salário Líquido */
+
+        
+        Console.WriteLine("Quanto você ganha por hora?");
+		double salarioHora = double.Parse(Console.ReadLine());
+		
+		Console.WriteLine("Número de horas trabalhadas no mês?");
+		double horaTrabalho = double.Parse(Console.ReadLine());
+		
+		double salarioBruto = salarioHora * horaTrabalho;
+		Console.WriteLine("O salário referido a este mês é de: R$ " + salarioBruto); //Salario Bruto
+		
+		double impostoRenda = (salarioBruto / 100) * 11; //IR de 11%
+		Console.WriteLine("IR (11%) : R$ " + impostoRenda);
+		
+		double Inss = (salarioBruto / 100)  * 8; // INSS de 8%
+		Console.WriteLine("INSS (8%) : R$: " + Inss);
+		
+		double Sindicato = (salarioBruto / 100)  * 5; //Sindicato 5%
+		Console.WriteLine("Sindicato (5%) : R$: " + Sindicato);
+		
+		double descontos = impostoRenda + Inss + Sindicato; // soma de todos os descontos 
+		double salarioLiquido = salarioBruto - descontos; //total Salário Liquido
+		
+		Console.WriteLine("Salário Liquido: " + salarioLiquido);	
+    }
+}
