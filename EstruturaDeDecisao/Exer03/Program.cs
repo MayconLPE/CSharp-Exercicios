5a83426b5751d24e5caa4d51d758555e9d9e6851
MayconL27 <mayconleandro2008@gmail.com>
2024-05-16 23:03:04 -0300

    Exercicios 03 e 04

---
+++ EstruturaDeDecisao/Exer03/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* 3. Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+        Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. */
+        const int MaxTamanhoString = 1;
+        Console.WriteLine("Digite F para Feminino ou M para Masculino");
+        string letra = Console.ReadLine();
+
+        if (letra == "f" || letra == "F")
+        {
+            Console.WriteLine("Feminino");
+        }
+        else if (letra == "m" || letra == "M")
+        {
+            Console.WriteLine("Masculino");
+        }
+        else
+        {
+            Console.WriteLine("Character incorreto");
+        }
+    }
+}
---
+++ EstruturaDeDecisao/Exer04/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+        Console.Write("Digite uma letra: ");
+        char letra = Console.ReadKey().KeyChar;
+
+        if (char.IsLetter(letra))
+        {
+            if (IsVogal(letra))
+                Console.WriteLine("\nÉ uma vogal.");
+            else
+                Console.WriteLine("\nÉ uma consoante.");
+        }
+        else
+        {
+            Console.WriteLine("\nNão é uma letra válida.");
+        }
+    }
+
+    static bool IsVogal(char c)
+    {
+        c = char.ToLower(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+}
